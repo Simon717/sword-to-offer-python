1bef2f8e674f31ca2f298011f4235ad7cf0e0f64
Simon717 <2711772037@qq.com>
2019-04-03 11:04:38 +0800

    update

--- 华为 蛇形字符串.py
+++ 华为 蛇形字符串.py
@@ -5,77 +5,14 @@
    Author :       simon
    date：          19-3-20
 """
-def solu(s):
-    s = list(s)
-    ss = []
-    letters = "abcdefghijklmnopqrstuvwxyz"
-
-    for i in s:
-        if i.lower() in letters:
-            ss.append(i)
-    ss.sort() # 原地操作
-    lower = []
-    upper = []
-    for i in ss:
-        if i in letters:
-            lower.append(i)
-        else:
-            upper.append(i)
-    merge = []
-    p1, p2 = 0, 0
-    while p1 < len(lower) and p2 < len(upper):
-        if lower[p1] >= upper[p2].lower():
-            merge.append(upper[p2])
-            p2 += 1
-        else:
-            merge.append(lower[p1])
-            p1 += 1
-    if p1 == len(lower):
-        merge += upper[p2:]
-    else:
-        merge += lower[p1:]
-
-    p1 = p2 = 0
-    maxlen = 0
-    res = []
-    reslen = []
-    while len(merge):
-        p1 = p2 = 0
-        maxlen = 0
-        cur = []
-        while p1 < len(merge)-1:
-            if ord(merge[p1]) - ord(merge[p1+1]) == 26:
-                i = 1
-                while ord(merge[p2]) - ord(merge[p2+1]) == 26 and ord(merge[p2]) - ord(merge[p1])==i:
-                    i += 1
-                    p2 += 2
-                curlen = (p2 - p1) // 2
-                if curlen > maxlen:
-                    maxlen = curlen
-                    cur = [p1, p2]
-                # res.append(merge[p1:p2])
-                # reslen.append((p2 - p1) // 2)
-            else:
-                p1 += 1
-                p2 += 1
-        res.append(merge[p1:p2])
-        for ii in range(p1, p2): # TODO
-            merge.pop(ii)
-    return res
 
 """
-本题思路： 
-1. 从输入字符串中过滤出我们需要的英文字母 按照大小写分离成两个部分 之后排序
-2. 首先只考虑大写字母（相应的小写字母是否存在只需要加上一行判断语句即可） 将问题转化为一个排序的字符串中找到最长的连续字符串
-3. 对于连续字符串问题 利用双指针思想 p1指向连续子序列的首字母 p2指向未字母 
-    这里我体会到 while 循环和for循环的不同之处和相同之处 while循环的有点在于一次性可以进行多重判断 因为这个问题的判断条件就是多重判断 所以while循环很方便
-    在每一次循环结束时人为加上index++ 跟for循环没有区别 for循环只是在固定循环次数的时候比较好用 当然可以用break短路
-    之前一直把while循环和if弄混.. 醉了
-
-Python
+重写
 """
-class Solution:
-    def she(self, s):
+from collections import Counter
+
+class Solution_:
+    def snack(self, s):
         s = list(s)
         upper, lower = [],  []
         for i in s:
@@ -83,77 +20,85 @@ class Solution:
                 lower.append(i)
             if ord('A') <= ord(i) <= ord('Z'):
                 upper.append(i)
-        upper.sort()
-        lower.sort()
+
+        cntUP = Counter(upper)
+        cntLo = Counter(lower)
+
+        def getChar():
+            for key in list(cntUP.keys()):
+                if key.lower() not in cntLo:  # 有A 无a 直接删掉A
+                    cntUP.pop(key)
+            res = list(cntUP.keys()) # 取出大写字母
+            return res
+
+        def delete(x):
+            x = ''.join(x)
+            for k in x:
+                cntUP[k] -= 1
+                if not cntUP[k]:
+                    # del cntUP[k] # 同样的效果
+                    cntUP.pop(k)
+
+            x = x.lower()
+            for k in x:
+                cntLo[k] -= 1
+                if not cntLo[k]:
+                    # del cntUP[k]
+                    cntLo.pop(k)
+
+
+        def maxLen(s):
+            # print(s)
+            res = s[0]
+            maxEnd = s[0]
+            for i in s[1:]:
+                # dp = dp + i if ord(i) - ord(dp[-1]) == 1 else i
+                if ord(i) - ord(maxEnd[-1]) == 1:
+                    maxEnd += i
+                else:
+                    maxEnd = i
+                if len(maxEnd) > len(res):
+                    res = maxEnd
+            return res
+
         res = []
+        while cntUP:
+            UPstr = getChar()
+            if not UPstr: break
 
-        while upper:
-            p1, p2 = 0, 0   # 某一轮搜索
-            step_maxlen = 0
-            step_res = []
-            while p1 < len(upper):  # 考察不同起点
-                if upper[p1].lower() in lower:
-                    p2 = p1 + 1
-                    p2Front = p1 # 定位到尾指针的前一个元素
-                    temp_res = [upper[p1]] # 以当前节点的作为起点的最长连续字符
-                    while p2 < len(upper) and (ord(upper[p2]) - ord(upper[p2Front]) in [0, 1]) and upper[p2].lower() in lower: # 考察不同的终点
-                        if ord(upper[p2]) - ord(upper[p2Front]) == 1:
-                            temp_res.append(upper[p2])
-                            p2Front = p2
-                        p2 += 1
-                    if len(temp_res) > step_maxlen:
-                        step_maxlen = len(temp_res)
-                        step_res = temp_res
-                else: # 找不到小写字符直接删除
-                    upper.remove(upper[p1])
-                p1 += 1
-            # 处理当前step得到的结果 并且删除掉已经使用了的字符
-            stepp = ''
-            for x in step_res:
-                stepp += x + x.lower()
-                upper.remove(x)
-                lower.remove(x.lower())
-            res.append(stepp)
-        return res
+            UPstr.sort()
 
-    def hashSolu(self, s):
-        upper, lower = {}, {}
-        for i in s:
-            if ord('A') <= ord(i) <= ord('Z'):
-                if i not in upper.keys():
-                    upper[i] = 0
-                upper[i] += 1
-            if ord('a') <= ord(i) <= ord('z'):
-                if i not in lower.keys():
-                    lower[i] = 0
-                lower[i] += 1
+            # 最长连续子序列问题
+            UPcontinue = maxLen(UPstr)
+            res.append(''.join(['{}{}'.format(x, x.lower()) for x in UPcontinue]))
 
-         self.dropUp(upper, lower)
-
-        while upper:
-            up_list = self.getUpList(upper)
-            maxlenUp = self.getMaxUp(up_list)
+            # 减计数  删键值
+            delete(UPcontinue)
+        print(res)
 
 
 
 
-    def dropUp(self, up, low):
-        return
-
-    def getUpList(self, up):
-        return
 
 
-"""
 
-"""
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
 
     # test = 'Aa'
     # test = 'OoPpBCccbAa'
+
     test = 'SxxxsrR*AaAaBbSsSs'
-    solu1 = Solution()
-    print(solu1.she(test))
+    solu1 = Solution_()
+    print(solu1.snack(test))
+
+    # print(maxLen('abcxxxxxabcdee'))
 
